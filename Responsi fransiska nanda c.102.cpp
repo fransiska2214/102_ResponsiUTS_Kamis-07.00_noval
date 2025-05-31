06c2d5a5a6cd16f27bc2494ed2f16424a3ec9648
fransiska2214 <2400018102@webmail.uad.ac.id>
2025-05-31 09:52:53 +0700

    Update Responsi fransiska nanda c.102.cpp

--- Responsi fransiska nanda c.102.cpp
+++ Responsi fransiska nanda c.102.cpp
@@ -41,30 +41,44 @@ cout<<"Mata kuliah: \n";
 }
 void simpanKeFile(jumlah.txt){
 	opstream (!file).empty();
-	
+	file.clase();
 }
 int main(){
 	string admin;
 	string mahasiswa;
 	string user,pass;
 	
-	
+
+if(pilih == 1 ){
+		cout << "Masukkan username: ";
+		cin >>user;
+}
+else if(pilih == 2 ){
+	cout <<"Masukkan password: ";
+	cin >>pass;
+}
 	do{
 		cout<<endl;
+		cout<<"======================== \n";
 		cout<<"Login Jadwal Mata Kuliah \n";
 		cout<<"Input Jadwal Mata Kuliah \n";
 		cout<<"Tampilkan jadwal Mata Kuliah \n";
 		cout<<"Keluar \n";
+		cout<<"======================== \n";
 		cout<<"Masukkan pilihan (1-4)"
 	}
 	switch(pilihan){
 		case 1:
-		cout << "Masukkan username: ";
-		cin >>user;
-		cout <<"Masukkan password: ";
-		cin >>pass;
+		login jadwal;
 		break;
 		case 2:
+		tampilan jadwal;
+		break;
+		case 4:
+		cout<<"Anda keluar dari program \n";
+		break;
+		default<< "Program bisa dijalankan \n" ;
+		break;
 	}
 	return 0;
 }
